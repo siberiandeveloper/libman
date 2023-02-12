9991d6cdba37e49ea6b87b91392ea4aaff26878b
Ivan <onemore04@gmail.com>
2023-02-12 16:36:59 +0700

    Initial commit

---
+++ src/test/java/tld/library/cool/CoolApplicationTests.java
@@ -0,0 +1,13 @@
+package tld.library.cool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
